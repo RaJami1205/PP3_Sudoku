b4ed7611eebfe0233794b5d049aa484a8182e6c1
RaJami1205 <rauljamiralfarorodriguez512@gmail.com>
2025-06-02 12:04:15 -0600

    Se implementó una interfaz de reglas del juego y se corrigió un botón no funcional

---
+++ sudoku/Pages/Instrucciones/instrucciones.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sudoku.Pages.Instrucciones
+{
+    public class instruccionesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
